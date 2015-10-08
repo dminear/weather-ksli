e577c36c20ab01023de72b5eb5a518f292f89f53
Daniel Minear <dan@minear.name>
2015-10-08 07:26:40 -0700

    Added cgi-bin script.

---
+++ getweather.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+#
+# This is a script that can be put in the cgi-bin directory of
+# a webserver to query Memcache and display the last updated
+# weather data.
+
+use Cache::Memcached;
+
+my $memd = new Cache::Memcached {
+        'servers' => [ "localhost:11211" ],
+        'debug' => 0,
+        };
+
+my $w = $memd->get("weather");
+
+print "Content-type: text/xml\n\n";
+
+print "$w" if $w;
+print "Nothing found\n" if ! $w;
